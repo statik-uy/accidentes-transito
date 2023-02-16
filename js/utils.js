9b919a3b14fb1354f96b3dd1edf1eb4ed52b767c
lucaspintos909 <lucaspintos909@gmail.com>
2023-02-15 22:39:12 -0300

    fix: Funcion para mostrar notificacion

--- js/utils.js
+++ js/utils.js
@@ -1,47 +1,52 @@
 "use strict";
 import { generateEmailTemplate } from "./generateEmailTemplate.js";
 
-const showNotification = (message) => {
+const showNotification = (message, type = "", callback = () => {}) => {
   const toastElement = document.getElementById("notification");
   const notificationMessage = document.getElementById("notification-message");
   const toast = new bootstrap.Toast(toastElement);
-  notificationMessage.innerHTML = message;
+
+  toastElement.addEventListener("hide.bs.toast", callback);
+
+  if (type == "success") {
+    toastElement.classList.remove("text-bg-light");
+    toastElement.classList.add("text-bg-success");
+    notificationMessage.innerHTML =
+      '<i class="fa fa-envelope me-2"></i>' + message;
+  } else if (type == "loading") {
+    toastElement.classList.remove("text-bg-success");
+    toastElement.classList.add("text-bg-light");
+    notificationMessage.innerHTML =
+      '<i class="fa fa-spinner me-2" aria-hidden="true"></i>' + message;
+  }
+
   toast.show();
 };
 
-const sendMail = (asunto, email, mensaje) => {
-  showNotification(
-    "Procesando solicitud."
+const sendMail = async (subject, email, message, successMessage) => {
+  showNotification("Procesando solicitud.", "loading");
+  const cuerpo = generateEmailTemplate(subject, message);
+  const res = await fetch(
+    "https://m76x7ulnva.execute-api.sa-east-1.amazonaws.com/sendMail",
+    {
+      mode: "no-cors",
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        asunto: subject,
+        mensaje: cuerpo,
+        para: ["lucaspintos909@gmail.com"],
+      }),
+    }
   );
-  const cuerpo = generateEmailTemplate(asunto, mensaje);
-  fetch("https://m76x7ulnva.execute-api.sa-east-1.amazonaws.com/sendMail", {
-    mode: "no-cors",
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      asunto: asunto,
-      mensaje: cuerpo,
-      para: ["lucaspintos909@gmail.com"],
-    }),
-  })
-    .then((response) => JSON.parse(response))
-    .then((data) => {
-      showNotification("La solicitud de contacto fue enviada!");
-      alert("La solicitud de contacto fue enviada!");
-      window.location.href = "/index.html";
-    })
-    .catch((error) => {
-      showNotification(
-        "La solicitud de contacto no pudo ser enviada, intente nuevamente."
-      );
-      console.error("Error:", error);
-    });
+  if (res) {
+    console.log(res);
+    showNotification(successMessage, "success");
+    /* window.location.href = "/index.html"; */
+  }
 };
-/* .then(function (response) {
-  return JSON.parse(response);
-}) */
 
 export { sendMail, showNotification };
